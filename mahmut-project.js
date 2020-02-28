d75c1138a42c38750f9237e0b961b6183484a653
brkysever <brkysever@gmail.com>
2020-02-28 15:14:32 +0300

    GhostProject

---
+++ mahmut-project.js
@@ -0,0 +1,64 @@
+const aa = document.querySelector('.aa');
+const btn = document.querySelector('.sor');
+const deleteButton = document.querySelector('.button');
+
+
+
+btn.addEventListener('click',function(e) {
+
+
+  const randomNumber = Math.floor(Math.random() *2);
+  
+  if(randomNumber){
+    document.querySelector(".zz").innerHTML="Evet";
+  } else {
+    document.querySelector(".xx").innerHTML="Hayır.";
+}
+  e.preventDefault();
+})
+
+
+
+const turtle = document.querySelector('.resim');
+let x = 0;
+let y = 0;
+const speed = 50;
+let flipped = false;
+let rotate = 0;
+function handleKeyDown(event) {
+  // if its not an arrow key, we dont care
+  if (!event.key.includes('Arrow')) { return; }
+  switch (event.key) {
+    case 'ArrowUp':
+      y = y - 1;
+      rotate = -90;
+      break;
+    case 'ArrowDown':
+      y = y + 1;
+      rotate = 90;
+      break;
+    case 'ArrowLeft':
+      x = x - 1;
+      rotate = 0;
+      flipped = true;
+      break;
+    case 'ArrowRight':
+      x = x + 1;
+      rotate = 0;
+      flipped = false;
+      break;
+    default:
+      console.log('That is not a valid move');
+      break;
+  }
+
+  turtle.setAttribute('style', `
+    --rotateX: ${flipped ? '180deg' : '0'};
+    --x: ${x * speed}px;
+    --y: ${y * speed}px;
+    --rotate: ${rotate}deg;
+  `);
+}
+window.addEventListener('keydown', handleKeyDown);
+
+
